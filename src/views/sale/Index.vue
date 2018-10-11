39ea90293753a3a8579a09dc3e2184926552b238
guojing <912518605@qq.com>
2018-10-11 17:22:24 +0800

    抽奖列表+抽奖记录

--- src/views/sale/Index.vue
+++ src/views/sale/Index.vue
@@ -1,10 +1,19 @@
 <template>
-    <div>抽奖</div>
+<div>
+    <router-view />
+</div>
 </template>
 
 <script>
-  export default {}
+
+	export default {
+		name: 'index'
+
+	}
 </script>
 
 <style>
+.contable {
+        margin: 20px 20px;
+    }
 </style>
